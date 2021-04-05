d3254e8190d1c5d8de892e2a038531b41ccef86e
Aleksey Yesin <esinru@gmail.com>
2021-04-05 08:05:43 +0300

    FeedbackOptions created, basic structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import { Component } from 'react';
-import './styles/App.module.scss';
+import css from './styles/App.module.scss';
+import FeedbackOptions from './components/FeedbackOptions';
 
 class App extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    };
+
+    handleLeaveFeedback(feedback) {
+        console.log(feedback);
+    }
+
     render() {
-        return <div></div>;
+        return (
+            <>
+                <h1 className={css.feedbackTitle}>Please leave feedback</h1>
+                <FeedbackOptions
+                    options={Object.keys(this.state)}
+                    onLeaveFeedback={this.handleLeaveFeedback}
+                ></FeedbackOptions>
+                <h2 className={css.statisticsTitle}>Statistics</h2>
+                {/* <Statistics good={} neutral={} bad={} total={} positivePercentage={}></Statistics> */}
+            </>
+        );
     }
 }
 
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,18 @@
+import css from './FeedbackOptions.module.scss';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+    <ul className={css.feedbackList}>
+        {options.map(feedback => (
+            <li key={feedback}>
+                <button
+                    onClick={() => onLeaveFeedback(feedback)}
+                    className={css.feedbackButton}
+                >
+                    {feedback}
+                </button>
+            </li>
+        ))}
+    </ul>
+);
+
+export default FeedbackOptions;
